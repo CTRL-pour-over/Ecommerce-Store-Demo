4672779ece1d7ec3e5b9e12891a324eceeaa68d4
Thomas <tventrone95@gmail.com>
2021-09-20 15:39:28 -0400

    remove key?

--- Ecommerce_Store/Ecommerce_Store/settings.py
+++ Ecommerce_Store/Ecommerce_Store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from . import secret_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n=z%qz+ck)w)ft_i$$z_7_rs85^94ylp-f!3_i@i00)i_#k!r9'
+SECRET_KEY = secret_settings.SECRET_KEY()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
